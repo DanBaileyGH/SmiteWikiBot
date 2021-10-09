1e7a27743f739414110c328816a61f9c29402969
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2021-10-09 18:02:22 +0100

    need perms to set start and mid messages now

--- commands/setmidguide.js
+++ commands/setmidguide.js
@@ -6,6 +6,13 @@ module.exports = {
     aliases: ["setmid"],
 	description: 'Set new mid guide command message',
 	execute(message, args) {
+        const server = client.guilds.cache.get('733765822127800391')
+        const member = server.members.cache.get(message.author.id)
+        const hasPerms = member ? member.roles.cache.get('895713618845384724') : false
+        if (!hasPerms) {
+            message.channel.send(new MessageEmbed().setDescription("You do not have permission to do this!")); 
+            return;
+        }
         setStartsMessage(message, args);
 	},
 };
