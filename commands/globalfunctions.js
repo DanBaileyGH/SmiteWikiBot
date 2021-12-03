6f0a5bbd5d5f6ce08fa5f6ec3fa0e4d052917e4e
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2021-12-03 18:30:06 +0000

    added mobile apostrophe

--- commands/globalfunctions.js
+++ commands/globalfunctions.js
@@ -77,7 +77,7 @@ function getAllObjectsOfType(type) {
 
 //valid types: god, item
 async function findObjectWithShortenedName(name, type) {
-    name = name.join(" ").replace(/ /g, "").replace("'", "").trim().toLowerCase();
+    name = name.join(" ").replace(/ /g, "").replace(/'/, "").replace(/’/g, "").trim().toLowerCase();
     const validTypes = ["god", "item"];
     if (!validTypes.includes(type)) {
         throw new Error(`Not a valid type of object - valid types: ${validTypes}`)
@@ -88,7 +88,7 @@ async function findObjectWithShortenedName(name, type) {
         let currentObjectName = "";
         objectList.forEach(object => {
             if (type == "god") {
-                currentObjectName = object.Name.replace(/ /g, "").replace(/'/g, "").trim().toLowerCase();
+                currentObjectName = object.Name.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
                 if (currentObjectName == name) {
                     let godObject = {
                         object: object,
@@ -97,7 +97,7 @@ async function findObjectWithShortenedName(name, type) {
                     resolve(godObject);
                 }
             } else if (type == "item") {
-                currentObjectName = object.DeviceName.replace(/ /g, "").replace(/'/g, "").trim().toLowerCase();
+                currentObjectName = object.DeviceName.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
                 if (currentObjectName == name) {
                     let itemObject = {
                         object: object,
@@ -113,7 +113,7 @@ async function findObjectWithShortenedName(name, type) {
         
         objectList.forEach(object => {
             if (type == "god") {
-                currentObjectName = object.Name.replace(/ /g, "").replace(/'/g, "").trim().toLowerCase();
+                currentObjectName = object.Name.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
                 if (currentObjectName.includes(name)) {
                     let godObject = {
                         object: object,
@@ -122,7 +122,7 @@ async function findObjectWithShortenedName(name, type) {
                     resolve(godObject);
                 }
             } else if (type == "item") {
-                currentObjectName = object.DeviceName.replace(/ /g, "").replace(/'/g, "").trim().toLowerCase();
+                currentObjectName = object.DeviceName.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
                 if (currentObjectName.includes(name)) {
                     let itemObject = {
                         object: object,
