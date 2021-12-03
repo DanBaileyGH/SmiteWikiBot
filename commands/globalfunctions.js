7e4a599e18e564d0eb1c9189d3d11c0b4b172a8c
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2021-12-03 18:40:04 +0000

    same issue different place

--- commands/globalfunctions.js
+++ commands/globalfunctions.js
@@ -77,7 +77,7 @@ function getAllObjectsOfType(type) {
 
 //valid types: god, item
 async function findObjectWithShortenedName(name, type) {
-    name = name.join(" ").replace(/ /g, "").replace(/'/, "").replace(/’/g, "").trim().toLowerCase();
+    name = name.join(" ").replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
     const validTypes = ["god", "item"];
     if (!validTypes.includes(type)) {
         throw new Error(`Not a valid type of object - valid types: ${validTypes}`)
