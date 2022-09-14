f21ba5d36e4061457dffc74dda13c2b95d5dc4d3
Daniel Bailey <74020155+DanBaileyGH@users.noreply.github.com>
2022-09-14 17:56:42 +0100

    Update botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -14,10 +14,7 @@ module.exports = {
         .setDescription(`Made With Discord.js, Uses The Smite API for God & Ability Information.
     All Builds from the Official Smite Server Mentor Team.
     Server Count: ${serverCount}.
-    Written and Maintained by DiscoFerry#6038 (ID 220922320938729472).
-    Source Code at https://github.com/DiscoFerry/SmiteWikiBot.
     Invite Me To Your Server With ?invite`)
-        .setFooter("Bot Still a WIP Currently.");
         
         const catchErr = err => {
             console.log(err)
