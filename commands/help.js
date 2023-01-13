40829a683b2bfee14ea82bfdc887d17a93817b0e
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2023-01-13 23:53:19 +0000

    removed midguide jungleguide start commands

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,6 @@ function sendHelpMessage(message) {
     .setTimestamp()
     .setFooter(`Bot Written and Maintained By DiscoFerry#6038`)
     .addField("**New Build Command**", "`?build [godname] (?b or ?[godname])` - Shows all mentor created builds for the chosen god\n Works without spaces, capitalisation, or apostrophes\n**Example usage:** ?b king arthur, ?arthur, ?chang, ?b zhong", false)
-    .addField("Other Build Commands", "`?start` - Shows all current meta starting builds for each role\n `?mid` - General guide for all mid lane and mage adc gods\n `?jungle` - General guide for all jungle gods", false)
     .addField("Wiki Commands", 
     "`?god [godname] (?g)` - Shows all stats for the chosen god.\n" +
     "`?abilities [godname] [(1,2,3,4,p,all)] (?a)` - Shows details for abilities of the chosen god.\n" +
