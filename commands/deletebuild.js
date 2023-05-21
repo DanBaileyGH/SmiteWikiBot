633341d01271b49225b2ed6543f20333990aa941
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2023-05-21 13:17:11 +0100

    reworked deletebuild.js

--- commands/deletebuild.js
+++ commands/deletebuild.js
@@ -1,57 +1,40 @@
-const fs = require('fs');
-const {MessageEmbed} = require('discord.js');
-const globalFunctions = require('./globalfunctions.js');
+const fs = require('fs')
+const { MessageEmbed } = require('discord.js')
+const globalFunctions = require('./globalfunctions.js')
 
 module.exports = {
 	name: 'deletebuild',
     aliases: ["db", "deleteb", "dbuild", "removebuild"],
 	description: 'Delete a specific mentor set build',
 	async execute(message, args) {
-        let hasPerms = await globalFunctions.userHasPerms(message);
+        const hasPerms = await globalFunctions.userHasPerms(message)
         if (!hasPerms) {
-            const embed = new MessageEmbed().setDescription("You do not have permission to do this here!");
-            message.channel.send({embeds: [embed]}); 
-            return;
+            const embed = new MessageEmbed().setDescription("You do not have permission to do this here!")
+            return ({embeds: [embed]}) 
         }
         if (args == "") { 
-            const embed = new MessageEmbed().setDescription("Please Enter a God");
-            message.channel.send({embeds: [embed]}); 
-            return;
+            const embed = new MessageEmbed().setDescription("Please Enter a God")
+            return ({embeds: [embed]}) 
         }
-        findBuild(message, args);
-	},
-};
+        findBuild(args)
+	}
+}
 
-function findBuild(message, buildId) {
-    let buildFound = false;
-    fs.readFile('builds.json', 'utf8', (err, buildsData) => {
-        if (err) {
-            console.log("File read failed: ", err);
-            return;
+async function findBuild(buildId) {
+    let buildFound = false
+    const buildsData = await fs.readFileSync('builds.json')
+    const buildList = await JSON.parse(buildsData)
+    for (build of buildList) {
+        if (build.id == buildId) {
+            buildFound = true
+            buildList.splice(buildList.indexOf(build), 1)
+            fs.writeFileSync('builds.json', JSON.stringify(buildList, null, 4))
+            const embed = new MessageEmbed().setDescription(`Build id ${build.id} deleted \nid ${build.id} was: ${build.role} ${build.god} build with items: ${build.items}`)
+            return ({embeds: [embed]})
         }
-        try {
-            buildList = JSON.parse(buildsData);
-        } catch (err) {
-            console.log("error parsing json string: ", err);
-            return;
-        }
-        buildList.forEach(build => {    
-            if (build.id == buildId) {
-                buildFound = true;
-                buildList.splice(buildList.indexOf(build), 1);
-                const data = JSON.stringify(buildList, null, 4);
-                fs.writeFile('builds.json', data, (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                })
-                const embed = new MessageEmbed().setDescription(`Build id ${build.id} deleted \nid ${build.id} was: ${build.role} ${build.god} build with items: ${build.items}`);
-                message.channel.send({embeds: [embed]});
-            }
-        });
-        if (!buildFound) {
-            const embed = new MessageEmbed().setDescription("Couldnt find a build with that id");
-            message.channel.send({embeds: [embed]});
-        }
-    });
+    }
+    if (!buildFound) {
+        const embed = new MessageEmbed().setDescription("Couldnt find a build with that id")
+        return ({embeds: [embed]})
+    }
 }
