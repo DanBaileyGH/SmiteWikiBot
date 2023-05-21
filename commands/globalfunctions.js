b46afb9d4785515514ee0bb5602fe5353449639b
DiscoFerry <74020155+DiscoFerry@users.noreply.github.com>
2023-05-21 15:02:49 +0100

    reworked globalfunctions.js

--- commands/globalfunctions.js
+++ commands/globalfunctions.js
@@ -1,161 +1,119 @@
-const md5 = require("md5");
-const config = require('../auth.json');
-const fs = require('fs');
-const {MessageEmbed} = require('discord.js');
+const md5 = require("md5")
+const fs = require('fs')
+const { MessageEmbed } = require('discord.js')
+const { devId, authKey } = require('../auth.json')
 
-const devId = config.devId;
-const authKey = config.authKey;
+const baseURL = "https://api.smitegame.com/smiteapi.svc/"
 
-const baseURL = "https://api.smitegame.com/smiteapi.svc/";
-
-const createSessionUrl = "createsessionJson/";
-const getGodsUrl = "getgodsjson/";
-const getItemsUrl = "getitemsjson/";
+const createSessionUrl = "createsessionJson/"
+const getGodsUrl = "getgodsjson/"
+const getItemsUrl = "getitemsjson/"
 const getGodSkinsUrl = "getgodskinsjson/"
 
 function getTimestamp() {
     const timestamp = (new Date()).toISOString().replace(/[^0-9]/g, "").slice(0, -3) //funnily enough i did not write this, thanks stack overflow
-    return timestamp;
+    return timestamp
 }
 
 function createSignature(method, timestamp) {
-    const signatureToHash = devId + method + authKey + timestamp;
-    const hashedSignature = md5(signatureToHash).toString();
-    return hashedSignature;
+    const signatureToHash = devId + method + authKey + timestamp
+    const hashedSignature = md5(signatureToHash).toString()
+    return hashedSignature
 }
 
-exports.generateCreateSessionUrl=()=>{
-    const timestamp = getTimestamp();
-    const reqURL = baseURL + createSessionUrl + devId + "/" + createSignature("createsession", timestamp) + "/" + timestamp;
-    return reqURL;
+exports.generateCreateSessionUrl = () => {
+    const timestamp = getTimestamp()
+    const reqURL = `${baseURL}${createSessionUrl}${devId}/${createSignature("createsession", timestamp)}/${timestamp}`
+    return reqURL
 }
 
-exports.generateGetGodsURL=(sessionId)=>{
-    const timestamp = getTimestamp();
-    const reqURL = baseURL + getGodsUrl + devId + "/" + createSignature("getgods", timestamp) + "/" + sessionId + "/" + timestamp + "/1";
-    return reqURL;
+exports.generateGetGodsURL = (sessionId) => {
+    const timestamp = getTimestamp()
+    const reqURL = `${baseURL}${getGodsUrl}${devId}/${createSignature("getgods", timestamp)}/${sessionId}/${timestamp}/1`
+    return reqURL
 }
 
-exports.generateGetItemsURL=(sessionId)=>{
-    const timestamp = getTimestamp();
-    const reqURL = baseURL + getItemsUrl + devId + "/" + createSignature("getitems", timestamp) + "/" + sessionId + "/" + timestamp + "/1";
-    return reqURL;
+exports.generateGetItemsURL = (sessionId) => {
+    const timestamp = getTimestamp()
+    const reqURL = `${baseURL}${getItemsUrl}${devId}/${createSignature("getitems", timestamp)}/${sessionId}/${timestamp}/1`
+    return reqURL
 }
 
-exports.generateGodSkinsUrl=(sessionId, godId)=>{
-    const timestamp = getTimestamp();
-    const reqURL = baseURL + getGodSkinsUrl + devId + "/" + createSignature("getgodskins", timestamp) + "/" + sessionId + "/" + timestamp + "/" + godId + "/1";
-    return reqURL;
+exports.generateGodSkinsUrl = (sessionId, godId) => {
+    const timestamp = getTimestamp()
+    const reqURL = `${baseURL}${getGodSkinsUrl}${devId}/${createSignature("getgodskins", timestamp)}/${sessionId}/${timestamp}/${godId}/1`
+    return reqURL
 }
 
-//valid types: god, item
-function getAllObjectsOfType(type) {
-    let objectsList = [];
-    return new Promise(resolve => {
-        fs.readFile(`${type}s.json`, 'utf8', (err, data) => {
-            if (err) {
-                console.log("File read failed: ", err);
-                return;
-            }
-            try {
-                dataList = JSON.parse(data);
-            } catch (err) {
-                console.log("error parsing json string: ", err);
-                return;
-            }
-            dataList.forEach(object => {
-                objectsList.push(object);
-            });
-            resolve(objectsList);
-        });
-    });
-}
-module.exports.getAllObjectsOfType = getAllObjectsOfType;
-
-//valid types: god, item
-async function findObjectWithShortenedName(name, type) {
-    name = name.join(" ").replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
-    const validTypes = ["god", "item"];
+exports.getAllObjectsOfType = async (type) => {
+    const validTypes = ["god", "item"]
     if (!validTypes.includes(type)) {
         throw new Error(`Not a valid type of object - valid types: ${validTypes}`)
     }
-    if (name == "amc") name = "ahmuzencab"; 
-    let objectList = await getAllObjectsOfType(type);
-    return new Promise(resolve => {
-        let currentObjectName = "";
-        objectList.forEach(object => {
-            if (type == "god") {
-                currentObjectName = object.Name.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
-                if (currentObjectName == name) {
-                    let godObject = {
-                        object: object,
-                        exact: true
-                    }
-                    resolve(godObject);
-                }
-            } else if (type == "item") {
-                currentObjectName = object.DeviceName.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
-                if (currentObjectName == name) {
-                    let itemObject = {
-                        object: object,
-                        objectList: objectList,
-                        exact: true
-                    }
-                    resolve(itemObject);
-                }
-            }
-        });
-        //didnt find exact match, now looking for abbreviations
-        if (name.length < 2) return;
-        
-        objectList.forEach(object => {
-            if (type == "god") {
-                currentObjectName = object.Name.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
-                if (currentObjectName.includes(name)) {
-                    let godObject = {
-                        object: object,
-                        exact: false
-                    }
-                    resolve(godObject);
-                }
-            } else if (type == "item") {
-                currentObjectName = object.DeviceName.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase();
-                if (currentObjectName.includes(name)) {
-                    let itemObject = {
-                        object: object,
-                        objectList: objectList,
-                        exact: false
-                    }
-                    resolve(itemObject);
-                }
-            }
-        });
-        resolve(false);
-    })
+    const objectsData = await fs.readFileSync(`${type}s.json`)
+    const objectsList = await JSON.parse(objectsData)
+    return(objectsList)
 }
-module.exports.findObjectWithShortenedName = findObjectWithShortenedName;
 
-exports.userHasPerms=(message)=>{
-    let hasPerms = false;
-    const serverId = message.guild.id;
-    return new Promise(resolve => {
-        if (serverId == config.smiteServerId) {
-            if (message.member.roles.cache.some(role => role.id == config.smiteServerPermsRoleId)) {
-                hasPerms = true;
+exports.findObjectWithShortenedName = async (name, type) => {
+    name = processNameString(name.join(" "))
+    if (name.length < 2) return
+    
+    const validTypes = ["god", "item"]
+    if (!validTypes.includes(type)) {
+        throw new Error(`Not a valid type of object - valid types: ${validTypes}`)
+    }
+
+    let objectList = await getAllObjectsOfType(type)
+    for (object of objectList) {
+        const currentObjectName = processNameString(object.Name)
+        if (currentObjectName == name) {
+            let returningObject = {
+                object: object,
+                exact: true
             }
-        } else if (serverId == config.testServerId) {
-            if (message.member.roles.cache.some(role => role.id == config.testServerPermsRoleId)) {
-                hasPerms = true;
+            if (type == "item") {
+                returningObject.objectList = objectList
             }
-        } else {
-            const embed = new MessageEmbed().setDescription("You cannot edit builds in this server!");
-            message.channel.send({embeds: [embed]}); 
+            return (returningObject)
         }
-        resolve(hasPerms)
-    });
+    }
+    
+    //didnt find exact match, now looking for abbreviations
+    for (object of objectList) {
+        const currentObjectName = processNameString(object.Name)
+        if (currentObjectName == name) {
+            let returningObject = {
+                object: object,
+                exact: false
+            }
+            if (type == "item") {
+                returningObject.objectList = objectList
+            }
+            return (returningObject)
+        }
+    }
+    return false
+}
+
+exports.userHasPerms = (message) => {
+    let hasPerms = false
+    if (serverId == config.smiteServerId) {
+        if (message.member.roles.cache.some(role => role.id == config.smiteServerPermsRoleId)) {
+            hasPerms = true
+        }
+    } else if (serverId == config.testServerId) {
+        if (message.member.roles.cache.some(role => role.id == config.testServerPermsRoleId)) {
+            hasPerms = true
+        }
+    } else {
+        const embed = new MessageEmbed().setDescription("You cannot edit builds in this server!")
+        return ({embeds: [embed]}) 
+    }
+    return (hasPerms)
 }
 
 exports.processNameString = (name) => {
-    const processedString = name.replace(/ /g, "").replace("'", "").trim().toLowerCase()
+    const processedString = name.replace(/ /g, "").replace(/'/g, "").replace(/’/g, "").trim().toLowerCase()
     return processedString
 }
